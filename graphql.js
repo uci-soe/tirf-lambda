332aa37f1e6bf740e7de1486dd2d1c232f970816
Rhett Lowe <rng2ml@gmail.com>
2020-02-18 16:01:56 -0800

    init

---
+++ graphql.js
@@ -0,0 +1,12 @@
+const { ApolloServer, makeExecutableSchema } = require('apollo-server-lambda');
+const { readFileSync }                       = require('fs');
+
+const context = require('./context-resolver');
+const schema  = makeExecutableSchema({
+  typeDefs:  readFileSync('./schema.graphql').toString(),
+  resolvers: require('./resolvers'),
+});
+
+const server = new ApolloServer({ schema });
+
+exports.graphqlHandler = server.createHandler();
