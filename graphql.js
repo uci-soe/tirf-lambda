ad0e7f51b00a7607d5cb144d31389296a0bba8ab
Rhett Lowe <rng2ml@gmail.com>
2020-02-19 16:26:23 -0800

    testy

--- graphql.js
+++ graphql.js
@@ -1,12 +1,25 @@
-const { ApolloServer, makeExecutableSchema } = require('apollo-server-lambda');
-const { readFileSync }                       = require('fs');
+// const { ApolloServer, makeExecutableSchema } = require('apollo-server-lambda');
+// const { readFileSync }                       = require('fs');
+//
+// const context = require('./context-resolver');
+// const schema  = makeExecutableSchema({
+//   typeDefs:  readFileSync('./schema.graphql').toString(),
+//   resolvers: require('./resolvers'),
+// });
+//
+// const server = new ApolloServer({ schema });
 
-const context = require('./context-resolver');
-const schema  = makeExecutableSchema({
-  typeDefs:  readFileSync('./schema.graphql').toString(),
-  resolvers: require('./resolvers'),
-});
+// exports.handler = server.createHandler();
+exports.handler = function (event, context) {
+  console.log(event);
+  context.succeed('hello');
+};
 
-const server = new ApolloServer({ schema });
 
-module.exports.handler = server.createHandler();
+// {
+//   database: process.env.DATABASE,
+//     host:     process.env.HOST,
+//   port:     process.env.PORT,
+//   user:     process.env.USERNAME,
+//   pass:     process.env.PASSWORD,
+// }
